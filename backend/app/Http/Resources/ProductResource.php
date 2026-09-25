baa10d73dd1602d1e00bb5e80eaa3ea681cb2e05
Phạm Hồng Gia Huy <236819006+phghuyit@users.noreply.github.com>
2026-09-25 14:08:51 +0700

    viet api cho producct

--- backend/app/Http/Resources/ProductResource.php
+++ backend/app/Http/Resources/ProductResource.php
@@ -15,22 +15,22 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'=> $this->id,
-            'category_id'=> $this->category_id,
-            'brand_id'=> $this->brand_id,
-            'name'=> $this->name,
-            'slug'=> $this->slug,
-            'short_description'=> $this->short_description,
-            'description'=> $this->description,
-            'rental_price_per_day'=> $this->rental_price_per_day,
-            'deposit_rate_percent'=> $this->deposit_rate_percent,
-            'original_value'=> $this->original_value,
-            'is_featured'=> $this->is_featured,
-            'view_count'=> $this->view_count,
-            'status'=> $this->status,
-            'category'=> new CategoryResource($this->whenLoaded('category')),
-            'brand'=> new BrandResource($this->whenLoaded('brand')),
-            
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'brand_id' => $this->brand_id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'short_description' => $this->short_description,
+            'description' => $this->description,
+            'rental_price_per_day' => $this->rental_price_per_day,
+            'deposit_rate_percent' => $this->deposit_rate_percent,
+            'original_value' => $this->original_value,
+            'is_featured' => $this->is_featured,
+            'view_count' => $this->view_count,
+            'status' => $this->status,
+            'category' => new CategoryResource($this->whenLoaded('category')),
+            'brand' => new BrandResource($this->whenLoaded('brand')),
+
         ];
     }
 }
